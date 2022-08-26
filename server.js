306922ddba6cca299c3a59f9f7309726506f01cf
Kevin Mahieu <82836519+kevdonche@users.noreply.github.com>
2022-08-26 16:34:09 +0200

    keycloak integration

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import  userRoutes  from './routes/usersRoute.js';
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
 
 const app = express();
 
@@ -11,6 +13,37 @@ app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', userRoutes);
+
+const memoryStore = new session.MemoryStore();
+app.use(
+  session({
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+  })
+);
+// 3
+const keycloak = new Keycloak({
+  store: memoryStore
+});
+app.use(
+  keycloak.middleware({
+    logout: '/logout',
+    admin: '/'
+  })
+);
+
+app.get('/api/unsecured', function(req, res) {
+    res.json({ message: 'This is an unsecured endpoint payload' });
+  });
+  // 4
+  app.get('/api/user', keycloak.protect('realm:user'), function(req, res) {
+    res.json({ message: 'This is an USER endpoint payload' });
+  });
+  app.get('/api/admin', keycloak.protect('realm:admin'), function(req, res) {
+    res.json({ message: 'This is an ADMIN endpoint payload' });
+  });
 
 const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.yf65c.mongodb.net/jenkinsUser?retryWrites=true&w=majority';
 const PORT = 4040;
